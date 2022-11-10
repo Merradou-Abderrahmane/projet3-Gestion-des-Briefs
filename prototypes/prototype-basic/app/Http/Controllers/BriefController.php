22aa743ac005e1da9e0cf7f0f0a1664f618978c4
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-11-10 02:34:29 +0100

    crud structure for both brief and tache

--- prototypes/prototype-basic/app/Http/Controllers/BriefController.php
+++ prototypes/prototype-basic/app/Http/Controllers/BriefController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Brief;
 
 class BriefController extends Controller
 {
     //
+    public function index()
+    {
+        $briefs = Brief::all();
+        // compact('Brief') is the same as ['briefs' => $briefs]
+        return view('index', ['briefs' => $briefs]);
+    }
+
+    public function create()
+    {
+        return view('addBrief');
+    }
+
+    public function store(Request $request)
+    {
+        $brief = new Brief();
+        $brief->briefName = $request->briefName;
+        $brief->save();
+        // return redirect('/');
+        return redirect('index');
+    }
+
+    public function edit($id)
+    {
+        $brief = Brief::find($id);
+        // return Brief::find($id);
+                                     #compact('Brief')
+        return view('editBrief', ['brief' => $brief] );
+    }
+
+    public function update(Request $request, $id)
+    {
+        $brief = Brief::find($id);
+        $brief->briefName = $request->input('briefName');
+        $brief->update();
+        // $Brief->save();
+        return redirect('index');    }
+
+    public function destroy($id)
+    {
+        $brief = Brief::find($id);
+        $brief->delete();
+        return redirect('index');
+    }
 }
+
--- prototypes/prototype-basic/app/Http/Controllers/TacheController.php
+++ prototypes/prototype-basic/app/Http/Controllers/TacheController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tache;
 
 class TacheController extends Controller
 {
     //
+    public function index()
+    {
+        $taches = Tache::all();
+        // compact('Tache') is the same as ['taches' => $taches]
+        return view('index', ['taches' => $taches]);
+    }
+
+    public function create()
+    {
+        return view('addTache');
+    }
+
+    public function store(Request $request)
+    {
+        $tache = new Tache();
+        $tache->tacheName = $request->tacheName;
+        $tache->save();
+        // return redirect('/');
+        return redirect('index');
+    }
+
+    public function edit($id)
+    {
+        $tache = Tache::find($id);
+        // return Tache::find($id);
+        return view('editTache', ['tache' => $tache] );
+    }
+
+    public function update(Request $request, $id)
+    {
+        $tache = Tache::find($id);
+        $tache->tacheName = $request->input('tacheName');
+        $tache->update();
+        // $Tache->save();
+        return redirect('index');
+    }
+
+    public function destroy($id)
+    {
+        $tache = Tache::find($id);
+        $tache->delete();
+        return redirect('index');
+    }
+
 }
